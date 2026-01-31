d5f08c9c1e2ac80d557be97b2ebddd175b7e1dc5
Justin Benjamin Kroh <122898766+KosmicKroh@users.noreply.github.com>
2026-01-31 01:38:08 -0600

    Updated Player with Fire and Water

--- baldknobbers-game/player.gd
+++ baldknobbers-game/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var waterTexture = load("res://Sprites/WaterDrop.png")
+var fireTexture = load("res://Sprites/Fireball.png")
+var isWater:bool = true
 
 
 func _physics_process(_delta: float) -> void:
@@ -13,7 +16,27 @@ func _physics_process(_delta: float) -> void:
 	
 	if directionX or directionY:
 		velocity = Vector2(directionX,directionY).normalized()*SPEED
+		if directionX == 1:
+			rotation = 0
+		elif directionX == -1:
+			rotation = PI
+		elif directionY == 1:
+			rotation = PI/2
+		elif directionY == -1:
+			rotation = -PI/2
 	else:
 		velocity *= 0.9
+	
+	if Input.is_action_just_pressed("add_water") and not $Particles.emitting == false:
+		$Particles.texture = waterTexture
+		$Particles.emitting = true
+		isWater = true
+		
+	if Input.is_action_just_pressed("add_fire") and $Particles.emitting == false:
+		$Particles.texture = fireTexture
+		$Particles.emitting = true
+		isWater = false
+	
+	$EffectArea.monitoring = $Particles.emitting
 
 	move_and_slide()
